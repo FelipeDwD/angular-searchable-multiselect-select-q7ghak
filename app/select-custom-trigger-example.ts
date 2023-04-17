10983d8986e2bb7e56213f89af27c1bc8c1a60a3
FelipeDwD <50635026+FelipeDwD@users.noreply.github.com>
2023-04-17 15:22:57 -0300

    Aplicando correção

--- app/select-custom-trigger-example.ts
+++ app/select-custom-trigger-example.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatSelect } from '@angular/material';
 import { Observable } from 'rxjs';
@@ -14,23 +14,14 @@ export class SelectCustomTriggerExample {
 
   @ViewChild('search') searchTextBox: ElementRef;
   @ViewChild('mySel') skillSel: MatSelect;  
-
-  config = {
-    inputPlaceholder: 'Selecionar propriedade(s)',
-    inputSearch: 'Buscar propriedades',
-    notFound: 'Nenhuma propriedade encontrada'
-  }
+  @Input() data: any
+  @Input() config: any
+  @Output() selectedValues: any  
 
   selectFormControl = new FormControl();
   searchTextboxControl = new FormControl();
-  selectedValues = [];
-  selectAll: boolean
-  data = [    
-    { id: 0, name: 'Selecionar todas' },
-    { id: 1, name: 'Hotel Hangar' },
-    { id: 2, name: 'Hotel Floph' },
-    { id: 3, name: 'Novo Vernon Hotel' }
-];
+  selectedItems = [];
+  selectAll: boolean  
 
   filteredOptions: Observable<any[]>;
 
@@ -40,65 +31,89 @@ export class SelectCustomTriggerExample {
         startWith<string>(''),
         map(name => this._filter(name))
       );
+      
+      this.data = [            
+        { id: 1, name: 'Hotel Hangar' },
+        { id: 2, name: 'Hotel Floph' },
+        { id: 3, name: 'Novo Vernon Hotel' },
+        { id: 4, name: 'Novo Vernon Resort' }
+    ];
+
+    this.config = {
+      inputPlaceholder: 'Selecionar propriedade(s)',
+      inputSearch: 'Buscar propriedades',
+      notFound: 'Nenhuma propriedade encontrada',
+      selectAllText: 'Selecionar todas'
+    }
+
+    if(this.data.length > 1)
+      this.data.unshift({ id: 0, name: this.config.selectAllText.toUpperCase() })      
   }
   
   private _filter(name: string){
     const filterValue = name.toLowerCase();        
-    this.selectFormControl.patchValue(this.selectedValues);
+    this.selectFormControl.patchValue(this.selectedItems);
     let filteredList = this.data.filter(option => option.name.toLowerCase().includes(filterValue));
-    return filteredList;
+    return name ? filteredList.filter(x => x.id != 0) : filteredList;
   }
 
   getSelectedsInfo() {
-    if (this.selectAll || this.selectedValues.length == this.data.length - 1)
+    if (this.selectAll || this.selectedItems.length == this.data.length - 1)
         return 'Todos Selecionados';
-    if (this.selectedValues.length == 1)
-        return this.selectedValues[0].name;
-    if (this.selectedValues.length >= 1)
-        return `${this.selectedValues[0].name} +${this.selectedValues.length - 1}`;
+    if (this.selectedItems.length == 1)
+        return this.selectedItems[0].name;
+    if (this.selectedItems.length >= 1)
+        return `${this.selectedItems[0].name} +${this.selectedItems.length - 1}`;
 }
 
  selectionChange(event) {     
   if (!event.isUserInput)
-      return;
+      return;    
 
   let id = event.source.value.id;
 
   if (id == 0) {
       if (!this.selectAll) {
           this.selectAll = true;
-          this.selectedValues = [];
-          this.selectedValues.push(...this.data);
+          this.selectedItems = [];
+          this.selectedItems.push(...this.data);
           this.skillSel.options.forEach((item) => item.select());
       }
       else {         
           this.selectAll = false;
-          this.selectedValues = [];
+          this.selectedItems = [];
           this.skillSel.options.forEach((item) => { item.deselect(); });
       }
+      this.selectedValues = this.selectedItems.filter(x => x.id != 0)
       return;
   }
 
-  let indexList = this.selectedValues.indexOf(event.source.value);
+  let indexList = this.selectedItems.indexOf(event.source.value);
   
   if (indexList == -1)
-      this.selectedValues.push(event.source.value);
+      this.selectedItems.push(event.source.value);
   else
-      this.selectedValues.splice(indexList, 1);
+      this.selectedItems.splice(indexList, 1);
 
 
-  if (this.selectedValues.filter(x => x.id != 0).length == this.data.length - 1) {
-      this.skillSel.options.first.select();
-      this.selectAll = true;
-      this.selectedValues.push(this.data[0]);
-  }else {
+  if (this.selectedItems.filter(x => x.id != 0).length == this.data.length - 1) {
+    this.selectAll = true;
+    this.selectedItems.push(this.data[0]);
+
+    if(this.skillSel.options.find(x => x.value.id == 0) != null)
+      this.skillSel.options.find(x => x.value.id == 0).select()      
+      
+  }else if(this.selectAll) {
       this.selectAll = false;
-      this.skillSel.options.first.deselect();  
-      let indexSelectAll = this.selectedValues.findIndex(x => x.id == 0)
+      let indexSelectAll = this.selectedItems.findIndex(x => x.id == 0)
 
       if(indexSelectAll != -1)
-        this.selectedValues.splice(indexSelectAll, 1)
+        this.selectedItems.splice(indexSelectAll, 1)
+
+      if(this.skillSel.options.find(x => x.value.id == 0) != null)
+        this.skillSel.options.find(x => x.value.id == 0).deselect()
   }
+  this.selectedValues = this.selectedItems.filter(x => x.id != 0)
 }
 
   openedChange(e) {            
@@ -112,4 +127,8 @@ export class SelectCustomTriggerExample {
     event.stopPropagation();
     this.searchTextboxControl.patchValue('');
   }
+
+  handleInput(event: KeyboardEvent): void{
+    event.stopPropagation();
+ }
 }
